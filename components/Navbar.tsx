f5e08d7646efc24a3786f8346e12742f492582a3
Daniela <martinezdl_@hotmail.com>
2023-01-14 19:05:33 -0300

    add padding and margin to elements in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,12 +2,17 @@ import { NextPage } from 'next';
 
 const Navbar: NextPage = () => {
     return (
-        <div>
-            <h1>Proyectos</h1>
-            <h1>Bolsa de talentos</h1>
-            <h1>Contacto</h1>
-            <h1>Nosotros</h1>
-            <button>Generar mi CV</button>
+        <div className='flex justify-between mt-8  mr-16 ml-28' >
+            <h1>LOGO</h1>
+            <div className='flex justify-end items-center'>
+                <h1 className='ml-11'>Proyectos</h1>
+                <h1 className='ml-11'>Bolsa de talentos</h1>
+                <h1 className='ml-11'>Contacto</h1>
+                <h1 className='ml-11'>Nosotros</h1>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white  py-2 px-4 rounded ml-11">
+                    Generar mi CV
+                </button>
+            </div>
         </div>
     )
 }
