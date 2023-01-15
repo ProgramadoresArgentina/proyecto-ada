e45bf3d72c2ff6da6d8414d91eec8edfe163ff33
Daniela <martinezdl_@hotmail.com>
2023-01-15 12:44:02 -0300

    add styles to button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,14 +2,14 @@ import { NextPage } from 'next';
 
 const Navbar: NextPage = () => {
     return (
-        <div className='flex justify-between mt-8  mr-16 ml-28' >
+        <div className='flex justify-between mt-8  mr-16 ml-28'  >
             <h1>LOGO</h1>
             <div className='flex justify-end items-center'>
-                <h1 className='ml-11'>Proyectos</h1>
-                <h1 className='ml-11'>Bolsa de talentos</h1>
-                <h1 className='ml-11'>Contacto</h1>
-                <h1 className='ml-11'>Nosotros</h1>
-                <button className="bg-indigo-500  text-white  py-2 px-4 rounded ml-11">
+                <h1 className='ml-11 font-normal'>Proyectos</h1>
+                <h1 className='ml-11 font-normal'>Bolsa de talentos</h1>
+                <h1 className='ml-11 font-normal'>Contacto</h1>
+                <h1 className='ml-11 font-normal'>Nosotros</h1>
+                <button className="bg-indigo-500   text-white font-semibold rounded-lg  py-2 px-4  ml-11">
                     Generar mi CV
                 </button>
             </div>
