e37428bf2e45e249ab8493ec55ddbb0d1deb80b5
Smael Nicolas <nico.smael@gmail.com>
2023-01-17 16:11:45 -0300

    PLAT-20 Add font family , colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				manrope: ["Manrope", "sans-serif"],
+				poppins: ["Poppins", "sans-serif"],
+			},
+			colors: {
+				grey: "#565656;",
+				blueStrong: "#2E3E5C;",
+				shadeBlue: "#607FF2;",
+				shadeBlueHover: "#4965cc;",
+			},
+		},
+	},
+	plugins: [],
 };
