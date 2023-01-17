bb3b32dfb4c2326e7f0d345e6822820abc75bf9d
Daniela <martinezdl_@hotmail.com>
2023-01-17 13:19:30 -0300

    Change H1 for Links in navbar, change button font-size

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,16 @@
 import { NextPage } from 'next';
+import Link from 'next/link';
 
 const Navbar: NextPage = () => {
     return (
-        <div className='flex justify-between mt-8  mr-16 ml-28'  >
-            <h1>LOGO</h1>
+        <div className='flex justify-between items-center mt-8  mr-16 ml-28'  >
+            <Link href="">LOGO</Link>
             <div className='flex justify-end items-center'>
-                <h1 className='ml-11 font-normal'>Proyectos</h1>
-                <h1 className='ml-11 font-normal'>Bolsa de talentos</h1>
-                <h1 className='ml-11 font-normal'>Contacto</h1>
-                <h1 className='ml-11 font-normal'>Nosotros</h1>
-                <button className="bg-indigo-500   text-white font-semibold rounded-lg  py-2 px-4  ml-11">
+                <Link href="" className='ml-11 font-normal'>Proyectos</Link>
+                <Link href="" className='ml-11 font-normal'>Bolsa de talentos</Link>
+                <Link href="" className='ml-11 font-normal'>Contacto</Link>
+                <Link href="" className='ml-11 font-normal'>Nosotros</Link>
+                <button className="bg-indigo-500 text-sm text-white rounded-lg  py-2 px-4  ml-11">
                     Generar mi CV
                 </button>
             </div>
