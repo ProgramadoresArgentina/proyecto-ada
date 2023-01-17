26a86077fd6aa6ff89752484a4d88e538c907086
Smael Nicolas <nico.smael@gmail.com>
2023-01-17 16:19:32 -0300

    PLAT-20 Created about paragraph for hero section

---
+++ components/Hero/Hero_Paragraph.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+export const Hero_Paragraph: FC = () => (
+	<p className="font-manrope w-full pr-2 sm:w-[450px] text-[20px] sm:text-[16px] leading-6 text-grey">
+		Somos una comunidad sin fines de lucro fundada en el 2018 con la idea de
+		compartir conocimientos y crear un lugar en común para programadores de
+		Argentina.
+	</p>
+);
