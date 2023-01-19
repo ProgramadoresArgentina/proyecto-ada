4888d3aeb294647a3bf3b955391a1cb3b3b33254
Ian Rosales <ianrosales634@gmail.com>
2023-01-19 09:12:12 -0300

    PLAT-15: constants/. add constants folder

---
+++ constants/index.ts
@@ -0,0 +1,7 @@
+export const navBarLinks = [
+  { name: 'Proyectos', path: '/' },
+  { name: 'Bolsa de Talentos', path: '/' },
+  { name: 'Contacto', path: '/' },
+  { name: 'Nosotros', path: '/' },
+  { name: 'Generar Mi CV', path: '/', buttonStyle: true },
+]
