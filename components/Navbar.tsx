009fcacca97c7366d9bd8a307f7ff2d1edf269a6
Daniela <martinezdl_@hotmail.com>
2023-01-20 13:26:43 -0300

    refactor links and add types

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,20 @@ import { useState } from 'react';
 import Image from 'next/image';
 import hamburgerIcon from '../public/hamburger.png';
 import Logo from '../public/LogoProgramadoresArgentina.png';
-const Navbar = () => {
+
+
+
+const Navbar: React.FC = () => {
+    let Links = [
+        { name: "Proyectos", path: "/" },
+        { name: "Bolsa de Talentos", path: "/" },
+        { name: "Contacto", path: "/" },
+        { name: "Nosotros", path: "/" },
+    ];
+    type Link = {
+        name: string
+        path: string
+    }
     let [open, setOpen] = useState(false);
     return (
         <div className='w-full sticky p-4'  >
@@ -26,10 +39,13 @@ const Navbar = () => {
 
                 {/* links */}
                 <ul className={`md:flex md:items-center p-4 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20' : 'top-[-490px]'}`}>
-                    <li className={`font-normal ${open ? 'ml-0 mt-4' : 'ml-6'}`}><a href="/">Proyectos</a></li>
-                    <li className={`font-normal ${open ? 'ml-0 mt-4' : 'ml-6'}`}><a href="/">Bolsa de talentos</a></li>
-                    <li className={`font-normal ${open ? 'ml-0 mt-4' : 'ml-6'}`}><a href="/">Contacto</a></li>
-                    <li className={`font-normal ${open ? 'ml-0 mt-4' : 'ml-6'}`}><a href="/">Nosotros</a></li>
+                    {
+                        Links.map((link: Link) => (
+                            <li key={link.name} className={`font-normal ${open ? 'ml-0 mt-4' : 'ml-6'}`}>
+                                <a href={link.path}>{link.name}</a>
+                            </li>
+                        ))
+                    }
                     <button className={`font-normal bg-indigo-500 text-sm text-white rounded-lg  py-2 px-4 ${open ? 'ml-0 mt-4' : 'ml-6'}`}>
                         <a href="/">Generar mi CV</a>
                     </button>
