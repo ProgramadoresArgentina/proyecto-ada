70c2aa2759d714f78fa9ed4ca6a402eecacc627a
Smael Nicolas <nico.smael@gmail.com>
2023-02-10 17:51:55 -0300

    PLAT - 29: types.tsx Added new interfaces for comments component

--- interface/types.ts
+++ interface/types.ts
@@ -2,3 +2,24 @@ export interface AvatarsProps {
 	followers: number;
 	members?: [];
 }
+import { ReactNode } from "react";
+
+export interface CommentProps {
+	date?: Date;
+	id?: number;
+	image: string;
+	name?: string;
+	text: string;
+}
+
+export interface ComentsSectionProps {
+	commentsList: CommentProps[];
+}
+
+export interface MarqueeCustomProps {
+	children: ReactNode[];
+	marqueeVelocity: number;
+	onFinish?: () => void;
+	onInit?: () => void;
+	scatterRandomly: boolean;
+}
