72c8cdea888ed03398903ad62f16b85a4d40b38a
Ian Rosales <ianrosales634@gmail.com>
2023-02-12 19:13:22 -0300

    PLAT-28: pages/index.tsx. add the projects Carousel Component into index.tsx file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
-import { NextPage } from "next";
-import { Hero } from "../components/Hero/Hero";
+import { NextPage } from 'next'
+import { Hero } from '../components/Hero/Hero'
+import ProjectsSection from '../components/ProjectsSection'
 
 const Home: NextPage = () => (
-		<div className="w-full min-h-screen">
-			<Hero />
-		</div>
-	);
+  <div className='w-full min-h-screen'>
+    <Hero />
+    <ProjectsSection />
+  </div>
+)
 
-export default Home;
+export default Home
