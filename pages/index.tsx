a9950ac1e4743baf5147a0fce6d7997c536f6809
Smael Nicolas <nico.smael@gmail.com>
2023-02-26 22:29:49 -0300

    PLAT-32: index.tsx - imported section contact to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,19 @@
-import { NextPage } from 'next'
-import { CommentsSection } from '../components'
-import { Hero } from '../components/Hero/Hero'
-import ProjectsSection from '../components/ProjectsSection'
-import StaffSection from '../components/StaffSection'
-import { commentsList } from '../data/dummy-data'
+import { NextPage } from "next";
+import { CommentsSection } from "../components";
+import { ContactSection } from "../components/Contact/ContactSection";
+import { Hero } from "../components/Hero/Hero";
+import ProjectsSection from "../components/ProjectsSection";
+import StaffSection from "../components/StaffSection";
+import { commentsList } from "../data/dummy-data";
 
 const Home: NextPage = () => (
-  <div className='w-full min-h-screen'>
-    <Hero />
-    <ProjectsSection />
-    <CommentsSection commentsList={commentsList} />
-    <StaffSection />
-  </div>
-)
+	<div className="w-full min-h-screen">
+		<Hero />
+		<ProjectsSection />
+		<CommentsSection commentsList={commentsList} />
+		<StaffSection />
+		<ContactSection />
+	</div>
+);
 
-export default Home
+export default Home;
