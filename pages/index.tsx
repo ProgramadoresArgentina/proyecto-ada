b3c3b58a49136a2eacad09b38f1b978a3bf63c36
Ian Rosales <ianrosales634@gmail.com>
2023-03-01 20:12:35 -0300

    PLAT-31: pages/index.tsx. modify index page, add the section staff component.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import { NextPage } from 'next'
 import { Hero } from '../components/Hero/Hero'
 import ProjectsSection from '../components/ProjectsSection'
+import StaffSection from '../components/StaffSection'
 
 const Home: NextPage = () => (
   <div className='w-full min-h-screen'>
     <Hero />
     <ProjectsSection />
+    <StaffSection />
   </div>
 )
 
