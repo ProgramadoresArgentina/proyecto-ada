5d8355b73792b9fb35ba0c87c69c67e8f8105821
Ian Rosales <ianrosales634@gmail.com>
2023-03-10 22:36:42 -0300

    PLAT-31: pages/index.tsx. modify index page, add the Staff section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,17 @@
-import { NextPage } from "next";
-import { CommentsSection } from "../components";
-import { Hero } from "../components/Hero/Hero";
-import ProjectsSection from "../components/ProjectsSection";
-import { commentsList } from "../data/dummy-data";
+import { NextPage } from 'next'
+import { CommentsSection } from '../components'
+import { Hero } from '../components/Hero/Hero'
+import ProjectsSection from '../components/ProjectsSection'
+import StaffSection from '../components/StaffSection'
+import { commentsList } from '../data/dummy-data'
 
 const Home: NextPage = () => (
-	<div className="w-full min-h-screen">
-		<Hero />
-		<ProjectsSection />
-		<CommentsSection commentsList={commentsList} />
-	</div>
-);
+  <div className='w-full min-h-screen'>
+    <Hero />
+    <ProjectsSection />
+    <CommentsSection commentsList={commentsList} />
+    <StaffSection />
+  </div>
+)
 
-export default Home;
+export default Home
