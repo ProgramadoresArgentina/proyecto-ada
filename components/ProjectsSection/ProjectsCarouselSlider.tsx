49d008dc5b0b5087f7cb245ab8649c7b3a1b92b3
Ian Rosales <ianrosales634@gmail.com>
2023-03-22 10:04:56 -0300

    PLAT-41: components/ProjectsSection/ProjectsCarouselSlider.tsx add snap-mandatory className

--- components/ProjectsSection/ProjectsCarouselSlider.tsx
+++ components/ProjectsSection/ProjectsCarouselSlider.tsx
@@ -6,7 +6,7 @@ export default function ProjectsCarouselSlider({ innerRef, data, sizeScreen, bre
       <div
         ref={innerRef}
         className={`flex gap-8 justify-start  items-center w-full transition-transform duration-500 
-        ${sizeScreen > breakpoint ? 'px-4 overflow-visible snap-none' : 'px-8 snap-x overflow-x-scroll no-scrollbar'}`}
+        ${sizeScreen > breakpoint ? 'px-4 overflow-visible snap-none' : 'px-8 snap-x snap-mandatory overflow-x-scroll no-scrollbar'}`}
       >
         {data.map(({ id, imageUrl, title, description, url }: any) => (
           <ProjectsCarouselCard key={id} id={id} imageUrl={imageUrl} title={title} description={description} url={url} />
