24afcecd618e01553173706a2453ed2da0dee74b
Smael Nicolas <nico.smael@gmail.com>
2023-04-06 21:35:30 -0300

    PLAT-42: ImageBg.tsx - Created background container for blog hero

---
+++ components/BlogPage/Hero/ImageBg.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+
+export const ImageBg: FC = () => (
+	<div
+		className="w-full h-full bg-background-blog-world bg-no-repeat bg-right-top bg-cover
+					xl:w-1/2"
+	/>
+);
