7a07bc035e1794c697d70d32646b105d4989080c
Smael Nicolas <nico.smael@gmail.com>
2023-04-06 21:37:52 -0300

    PLAT-42: index.tsx - Created route to blog and imported hero section

---
+++ pages/blog/index.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+import { Hero } from "../../components/BlogPage/Hero/Hero";
+
+const Blog: NextPage = () => (
+	<div className="w-full min-h-screen">
+		<Hero />
+	</div>
+);
+export default Blog;
