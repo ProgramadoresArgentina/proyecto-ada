c92ee9b9ecb90da8c718c12f491dbd3337fbd133
Ian Rosales <ianrosales634@gmail.com>
2023-04-16 20:04:04 -0300

    PLAT 47: constants/quilljs.config.ts add quilljs config

--- constants/constants.tsx
+++ constants/constants.tsx
@@ -1,10 +1,14 @@
 export const LINKS_URL_SOCIALS = {
-	facebook: "https://www.facebook.com/programadoresargentina",
-	linkedin: "https://www.linkedin.com/company/programadores-argentina/",
-	github: "https://github.com/ProgramadoresArgentina",
-	instagram: "https://www.instagram.com/programadores_argentina",
-	email: "programadoresargentina@gmail.com",
-	discord: "https://discord.gg/H3uQmYF6Xh",
-	youtube: "",
-	twitter: "",
-};
+  facebook: "https://www.facebook.com/programadoresargentina",
+  linkedin: "https://www.linkedin.com/company/programadores-argentina/",
+  github: "https://github.com/ProgramadoresArgentina",
+  instagram: "https://www.instagram.com/programadores_argentina",
+  email: "programadoresargentina@gmail.com",
+  discord: "https://discord.gg/H3uQmYF6Xh",
+  youtube: "",
+  twitter: "",
+}
+
+export const CREATE_ARTICLE_ERROR_TYPES = {
+  TITLE_REQUIRED: 1,
+}
---
+++ constants/quilljs.config.ts
@@ -0,0 +1,37 @@
+export const toolbarOptions = [
+  ['bold', 'italic', 'underline', 'strike', 'code', 'link'], // toggled buttons
+  ['image', 'code-block', 'video'],
+
+  [{ list: 'ordered' }, { list: 'bullet' }],
+  [{ script: 'sub' }, { script: 'super' }],
+  [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
+
+  [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
+
+  [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+  [{ font: [] }],
+  [{ align: [] }],
+]
+export const formatsOptions = [
+  'background',
+  'bold',
+  'color',
+  'font',
+  'code',
+  'italic',
+  'link',
+  'size',
+  'strike',
+  'script',
+  'underline',
+  'blockquote',
+  'header',
+  'indent',
+  'list',
+  'align',
+  'direction',
+  'code-block',
+  'formula',
+  'image',
+  'video',
+]
