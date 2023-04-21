928879005b20f15a791b3d2379f3a56108c29a2f
Daniela <martinezdl_@hotmail.com>
2023-04-21 16:09:22 -0300

    order props alphabetically

--- components/FormComponents/DowloadFormBtn.tsx
+++ components/FormComponents/DowloadFormBtn.tsx
@@ -6,10 +6,9 @@ interface Props {
 const DownloadFormBtn: React.FC<Props> = ({ onClick }) => {
     return (
         <button
-            type="button"
-            className="bg-shadeBlue  
-            rounded w-48 h-10 content-center justify-center px-2 m-2"
+            className="bg-shadeBlue rounded w-48 h-10 content-center justify-center px-2 m-2"
             onClick={onClick}
+            type="button"
         >
             <p className="text-white text-xs font-normal mx-1">Descargar</p>
         </button >
