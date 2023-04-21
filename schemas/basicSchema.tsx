8a1c098baa24a7386d45677137d42b89f32c3e09
Daniela <martinezdl_@hotmail.com>
2023-04-21 15:54:22 -0300

    delete comments

--- schemas/basicSchema.tsx
+++ schemas/basicSchema.tsx
@@ -1,15 +1,8 @@
 import * as yup from "yup";
 
-// const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/;
-// min 5 characters, 1 upper case letter, 1 lower case letter, 1 numeric digit.
-
 export const basicSchema = yup.object().shape({
-    // email: yup.string().email("Please enter a valid email").required("Required"),
-    // age: yup.number().positive().integer().required("Required")
-
     basic: yup.array().of(
         yup.object().shape({
-            //   name: Yup.string().required("Name required"),
             email: yup.string().required("email required").email("Ingresa un email valido"),
             name: yup.string().required("Ingresa tu nombre"),
             position: yup.string().required("Ingresa tu puesto")
