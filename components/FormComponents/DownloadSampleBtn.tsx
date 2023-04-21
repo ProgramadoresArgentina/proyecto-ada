661731bd78a423a87952c18151475943934b6692
Daniela <martinezdl_@hotmail.com>
2023-04-21 16:10:17 -0300

    order props alphabetically

--- components/FormComponents/DownloadSampleBtn.tsx
+++ components/FormComponents/DownloadSampleBtn.tsx
@@ -8,17 +8,17 @@ interface Props {
 const DownloadBtn: React.FC<Props> = ({ onClick }) => {
     return (
         <button
-            type="button"
             className="bg-shadeBlue  
             rounded w-50 h-12 content-center justify-center px-2 
             absolute z-1 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
             onClick={onClick}
+            type="button"
         >
             <p className=" flex flex-inline text-white text-xs font-normal mx-1">
                 <span className='mx-1'>
                     <Image
-                        src={eye}
                         alt="eye"
+                        src={eye}
                     />
                 </span>
                 Descargar ejemplo
