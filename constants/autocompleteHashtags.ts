43eda42f7992d2f3fc4aadb9b600d9d2410dee20
Ian Rosales <ianrosales634@gmail.com>
2023-04-25 11:49:40 -0300

    PLAT 47: constants/autocompleteHashtags.ts add the autocompleteHashtagsn list

---
+++ constants/autocompleteHashtags.ts
@@ -0,0 +1,498 @@
+export const AUTOCOMPLETE_HASHTAGS = [
+  {
+    id: 1,
+    title: 'webdev',
+    description: 'Herramienta CASE que permite crear sitios Web. Parecido a WinDev, fue editado por PC SOFT'
+  },
+  {
+    id: 2,
+    title: 'javascript',
+    description: 'Dialecto del estándar ECMAScript. orientado a objetos, basado en prototipos, débilmente tipado...'
+  },
+  {
+    id: 3,
+    title: 'developer',
+    description: 'Idear el conjunto de órdenes que llevan a un sistema informático a realizar una acción...'
+  },
+  {
+    id: 4,
+    title: 'healthydebate',
+    description: 'Construye el desarrollo de la habilidad para expresar ideas de manera argumentada...'
+  },
+  {
+    id: 5,
+    title: 'technology',
+    description: 'Transformar o combinar algo ya existente para construir algo nuevo o bien darle otra función...'
+  },
+  {
+    id: 6,
+    title: 'frontend',
+    description: 'Persona encargada de programar el navegador de una web...'
+  },
+  {
+    id: 7,
+    title: 'coding',
+    description: 'Se encargan de enseñar las habilidades más importantes en la programación...'
+  },
+  {
+    id: 8,
+    title: 'java',
+    description: 'Lenguaje de programación creado por Sun Microsystems en 1995...'
+  },
+  {
+    id: 9,
+    title: 'python',
+    description: 'Utilizado en el desarrollo de software, la ciencia de datos, el machine learning y más...'
+  },
+  {
+    id: 10,
+    title: 'php',
+    description: 'Permite el desarrollo web o aplicaciones web dinámicas...'
+  },
+  {
+    id: 11,
+    title: 'tutorial',
+    description: 'Método de transferencia de conocimiento...'
+  },
+  {
+    id: 12,
+    title: 'react',
+    description: 'Te ayuda a crear interfaces de usuario interactivas de forma sencilla...'
+  },
+  {
+    id: 13,
+    title: 'angular',
+    description: 'Framework de ingeniería de software de código abierto mantenido por Google...'
+  },
+  {
+    id: 14,
+    title: 'vite',
+    description: 'Herramienta de compilación...'
+  },
+  {
+    id: 15,
+    title: 'nodejs',
+    description: 'Entorno de ejecución JavaScript de código abierto y multiplataforma...'
+  },
+  {
+    id: 16,
+    title: 'deno',
+    description: 'Runtime para JavaScript y TypeScript basado en el motor V8 y escrito en Rust....'
+  },
+  {
+    id: 17,
+    title: 'aws',
+    description: 'Colección de servicios de computación en la nube...'
+  },
+  {
+    id: 18,
+    title: 'devops',
+    description: 'Marco de trabajo y una filosofía en constante evolución...'
+  },
+  {
+    id: 19,
+    title: 'opensource',
+    description: 'Código Abierto diseñado de manera que sea accesible al público...'
+  },
+  {
+    id: 20,
+    title: 'css',
+    description: 'Maneja el diseño y presentación de las páginas web...'
+  },
+  {
+    id: 21,
+    title: 'typescript',
+    description: 'Lenguaje de programación libre y de código abierto desarrollado por Microsoft...'
+  },
+  {
+    id: 22,
+    title: 'mysql',
+    description: 'Permite almacenar y acceder a los datos a través de múltiples motores de almacenamiento...'
+  },
+  {
+    id: 23,
+    title: 'postgresql',
+    description: 'Sistema de bases de datos compatible la mayoría de las herramientas más populares del mercado...'
+  },
+  {
+    id: 24,
+    title: 'github',
+    description: 'Permite subir repositorios de código para almacenarlo en el sistema de control de versiones Git...'
+  },
+  {
+    id: 25,
+    title: 'html',
+    description: 'Códigos utilizado para "marcar" el texto de una página web...'
+  },
+  {
+    id: 26,
+    title: 'backend',
+    description: 'Parte de la app que el usuario final no puede ver...'
+  },
+  {
+    id: 27,
+    title: 'fullstack',
+    description: 'Profesional que cuenta con diversas habilidades informáticas...'
+  },
+  {
+    id: 28,
+    title: 'database',
+    description: 'Almacén o bóveda en donde el sistema guarda la información seleccionada por el usuario...'
+  },
+  {
+    id: 29,
+    title: 'news',
+    description: 'Organiza, busca y lee noticias...'
+  },
+  {
+    id: 30,
+    title: 'rust',
+    description: 'Lenguaje de programación, se usa para escribir sistemas (como sistemas operativos)...'
+  },
+  {
+    id: 31,
+    title: 'api',
+    description: 'Conjunto de definiciones y protocolos, se usa para diseñar e integrar el software de las aplicaciones...'
+  },
+  {
+    id: 32,
+    title: 'android',
+    description: 'Sistema operativo móvil basado en el núcleo Linux y otros software de código abierto...'
+  },
+  {
+    id: 33,
+    title: 'mobile',
+    description: 'Resolver problemas a través de aplicaciones y/o plataformas móviles...'
+  },
+  {
+    id: 34,
+    title: 'desktop',
+    description: 'Aplicaciones y programas especializados para ordenadores de mesa y computadoras portátiles...'
+  },
+  {
+    id: 35,
+    title: 'gaming',
+    description: 'Diseñador y desarrollador de videojuegos...'
+  },
+  {
+    id: 36,
+    title: 'docker',
+    description: 'Automatiza el despliegue de aplicaciones dentro de contenedores de software...'
+  },
+  {
+    id: 37,
+    title: 'blockchain',
+    description: 'Servicio de exploración de bloques de Bitcoin...'
+  },
+  {
+    id: 38,
+    title: 'linux',
+    description: 'Sistema operativo open source, lanza en virtud de la Licencia Pública General de GNU (GPL)...'
+  },
+  {
+    id: 39,
+    title: 'laravel',
+    description: 'Framework de código abierto para desarrollar aplicaciones y servicios web con PHP...'
+  },
+  {
+    id: 40,
+    title: 'vscode',
+    description: 'Editor de código fuente desarrollado por Microsoft para Windows, Linux, macOS y Web...'
+  },
+  {
+    id: 41,
+    title: 'ios',
+    description: 'Sistema operativo móvil de código cerrado desarrollado por Apple Inc...'
+  },
+  {
+    id: 42,
+    title: 'gpt-4',
+    description: 'Modelo de lenguaje de gran tamaño creado por OpenAI...'
+  },
+  {
+    id: 43,
+    title: 'chatgpt',
+    description: 'Inteligencia artificial desarrollado en 2022 por OpenAI que se especializa en el diálogo...'
+  },
+  {
+    id: 44,
+    title: 'ia',
+    description: 'Disciplina y un conjunto de capacidades cognoscitivas e intelectuales expresadas por sistemas informáticos...'
+  },
+  {
+    id: 45,
+    title: 'nextjs',
+    description: 'Marco web de desarrollo front-end de React de código abierto creado por Vercel...'
+  },
+  {
+    id: 46,
+    title: 'mongodb',
+    description: 'Sistema de base de datos NoSQL, orientado a documentos y de código abierto...'
+  },
+  {
+    id: 47,
+    title: 'mariadb',
+    description: 'Sistema de gestión de bases de datos derivado de MySQL con licencia GPL...'
+  },
+  {
+    id: 48,
+    title: 'firebase',
+    description: 'Plataforma para el desarrollo de aplicaciones web y aplicaciones móviles...'
+  },
+  {
+    id: 49,
+    title: 'ux',
+    description: 'Diseño de experiencia de usuario o UX design...'
+  },
+  {
+    id: 50,
+    title: 'ui',
+    description: 'Interfaces de usuario, entornos en pantalla fáciles de usar, atractivos e interesantes...'
+  },
+  {
+    id: 51,
+    title: 'performance',
+    description: 'Grado de agilidad y respuesta con que se desempeña un sitio web...'
+  },
+  {
+    id: 52,
+    title: 'gamedev',
+    description: 'Desarrollador de software que crea videojuegos para diversas plataformas...'
+  },
+  {
+    id: 53,
+    title: 'dev',
+    description: 'Diseña programas de ordenador y aplicaciones web sirviéndose de un lenguaje de programación...'
+  },
+  {
+    id: 54,
+    title: 'design',
+    description: 'Dar estructura a un objeto de diseño ya sea tangible o intangible...'
+  },
+  {
+    id: 55,
+    title: 'ruby',
+    description: 'Lenguaje de programación interpretado, reflexivo y orientado a objetos...'
+  },
+  {
+    id: 56,
+    title: 'bash',
+    description: 'Interfaz de usuario de línea de comandos popular...'
+  },
+  {
+    id: 57,
+    title: 'graphql',
+    description: 'Lenguaje de consulta y manipulación de datos para APIs...'
+  },
+  {
+    id: 58,
+    title: 'redux',
+    description: 'Librería JavaScript de código abierto para el manejo del estado de las aplicaciones...'
+  },
+  {
+    id: 59,
+    title: 'atlassian',
+    description: 'Empresa de software con sede en Australia...'
+  },
+  {
+    id: 60,
+    title: 'trello',
+    description: 'Software de administración de proyectos con interfaz web y con cliente para iOS y android...'
+  },
+  {
+    id: 61,
+    title: 'jirasoftware',
+    description: 'Software propietario para la gestión de proyectos, seguimiento de errores e incidencias...'
+  },
+  {
+    id: 62,
+    title: 'confluence',
+    description: 'Software de colaboración en equipo. Escrito en Java...'
+  },
+  {
+    id: 63,
+    title: 'git',
+    description: 'Software de control de versiones diseñado por Linus Torvalds...'
+  },
+  {
+    id: 64,
+    title: 'scrum',
+    description: 'Marco de gestión de proyectos de metodología ágil...'
+  },
+  {
+    id: 65,
+    title: 'expressjs',
+    description: 'Entorno de trabajo para aplicaciones web para el programario Node.js...'
+  },
+  {
+    id: 66,
+    title: 'vuejs',
+    description: 'Framework de JavaScript de código abierto para la construcción de interfaces de usuario...'
+  },
+  {
+    id: 67,
+    title: 'framework',
+    description: 'Entorno de trabajo o marco de trabajo es un conjunto estandarizado de conceptos...'
+  },
+  {
+    id: 68,
+    title: 'it',
+    description: 'Tecnologías de la información...'
+  },
+  {
+    id: 69,
+    title: 'robots',
+    description: 'Máquinas pensantes que se sublevan y terminan por matar a su creador...'
+  },
+  {
+    id: 70,
+    title: 'artificialintelligence',
+    description: 'Rama de la computación que busca simular la inteligencia humana en una máquina...'
+  },
+  {
+    id: 71,
+    title: 'software',
+    description: 'Logicial o soporte lógico al sistema formal de un sistema informático...'
+  },
+  {
+    id: 72,
+    title: 'ai',
+    description: 'Rama de la computación que busca simular la inteligencia humana en una máquina...'
+  },
+  {
+    id: 73,
+    title: 'productivity',
+    description: 'Llevar a cabo las tareas laborales maximizando la eficiencia...'
+  },
+  {
+    id: 74,
+    title: 'testing',
+    description: 'Evaluar la calidad del producto, y mejorarlo al identificar defectos y problemas...'
+  },
+  {
+    id: 75,
+    title: 'test',
+    description: 'Pruebas destinadas a evaluar conocimientos, aptitudes o funciones...'
+  },
+  {
+    id: 76,
+    title: 'postman',
+    description: 'Plataforma API para que los desarrolladores diseñen, construyan, prueben e iteren sus API...'
+  },
+  {
+    id: 78,
+    title: 'jest',
+    description: 'Marco de prueba de JavaScript creado sobre Jasmine y mantenido por Meta...'
+  },
+  {
+    id: 79,
+    title: 'architecture',
+    description: 'Patrones y abstracciones coherentes, interactuar con el código fuente del software...'
+  },
+  {
+    id: 80,
+    title: 'sass',
+    description: 'Preprocesador de CSS compatible con todas sus versiones...'
+  },
+  {
+    id: 81,
+    title: 'less',
+    description: 'Lenguaje de hojas de estilo que puede ser compilado en CSS...'
+  },
+  {
+    id: 82,
+    title: 'styledcomponents',
+    description: 'Biblioteca de javascript usada en React, crear componentes de estilización personalizados...'
+  },
+  {
+    id: 83,
+    title: 'sql',
+    description: 'Lenguaje de computación para trabajar con conjuntos de datos y las relaciones entre ellos...'
+  },
+  {
+    id: 84,
+    title: 'server',
+    description: 'Conjunto de computadoras capaces de atender las peticiones de un cliente...'
+  },
+  {
+    id: 85,
+    title: 'django',
+    description: 'Framework de desarrollo web de código abierto, escrito en Python...'
+  },
+  {
+    id: 86,
+    title: 'go',
+    description: 'Lenguaje de programación, con tipado estático inspirado en la sintaxis de C...'
+  },
+  {
+    id: 87,
+    title: 'c#',
+    description: 'Lenguaje de programación multiparadigma desarrollado y estandarizado por Microsoft...'
+  },
+  {
+    id: 88,
+    title: 'c/c++',
+    description: 'Lenguaje de programación diseñado en 1979 por Bjarne Stroustrup...'
+  },
+  {
+    id: 89,
+    title: 'r',
+    description: 'Lenguaje de programación con un enfoque al análisis estadístico...'
+  },
+  {
+    id: 90,
+    title: 'objective-c',
+    description: 'Lenguaje de programación orientado a objetos creado como un superconjunto de C...'
+  },
+  {
+    id: 91,
+    title: 'swift',
+    description: 'Lenguaje de programación multiparadigma creado por Apple...'
+  },
+  {
+    id: 92,
+    title: 'matlab',
+    description: 'Está disponible para las plataformas Unix, Windows, macOS y GNU/Linux...'
+  },
+  {
+    id: 93,
+    title: 'flutter',
+    description: 'SDK de código fuente abierto de desarrollo de aplicaciones móviles creado por Google...'
+  },
+  {
+    id: 94,
+    title: 'ionic',
+    description: 'SDK completo de código abierto para el desarrollo de aplicaciones móviles híbridas...'
+  },
+  {
+    id: 95,
+    title: 'vercel',
+    description: 'Plataforma de nube estadounidense como empresa de servicios...'
+  },
+  {
+    id: 96,
+    title: 'reactnative',
+    description: 'Framework de código abierto creado por Meta Platforms, Inc...'
+  },
+  {
+    id: 97,
+    title: 'native',
+    description: 'Cualquier aplicación escrita para funcionar en una plataforma de dispositivo nativa...'
+  },
+  {
+    id: 98,
+    title: 'apache',
+    description: 'Servidor web HTTP de código abierto...'
+  },
+  {
+    id: 99,
+    title: '.net',
+    description: 'Plataforma de código abierto para crear aplicaciones de escritorio, web y móviles...'
+  },
+  {
+    id: 100,
+    title: 'nestjs',
+    description: 'Framework Node.js de servidor, ideal para crear aplicaciones de backend...'
+  },
+
+]
