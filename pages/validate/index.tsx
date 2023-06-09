5a88d1e81f8e90c2988dff340624967393d075b1
Smael Nicolas <nico.smael@gmail.com>
2023-06-09 15:52:19 -0300

    PLAT-51: index.tsx - page to validate users logged from auth0

---
+++ pages/validate/index.tsx
@@ -0,0 +1,62 @@
+// import { useUser } from "@auth0/nextjs-auth0/client";
+import { NextPage } from "next";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/router";
+import { useEffect, useRef } from "react";
+import { useFetch } from "../../hooks/useFetch";
+
+const Validate: NextPage = () => {
+	const { push } = useRouter();
+	// const { user } = useUser();
+	const divMessageRef = useRef(null);
+	const searchParams = useSearchParams();
+	const urlParam = searchParams.get("redirect");
+	const { data, loading, error, fetchData } = useFetch();
+
+	const updateStatus = (value: string, redirect: string) => {
+		divMessageRef.current.innerText = value;
+		setTimeout(() => {
+			push(redirect);
+		}, 2000);
+	};
+
+	useEffect(() => {
+		if (error)
+			// updateStatus(`Error al validar , redireccionando a Home`, error.redirectTo);
+			updateStatus(`Error al validar , redireccionando a Home`, "/");
+		if (data)
+			// updateStatus(`Usuario valido , redireccionando a ${param}`, data.redirectTo);
+			updateStatus(
+				`Usuario valido , redireccionando a ${urlParam}`,
+				urlParam
+			);
+
+		fetchData("https://jsonplaceholder.typicode.com/posts", "POST", {
+			userId: 24,
+			title: "Titulo Test",
+			body: "Body Test",
+		});
+
+		// fetchData("https://jsonplaceholder.typicode.com/posts");
+	}, [loading]);
+
+	return (
+		<div className="w-full min-h-screen flex justify-start flex-col items-center gap-6 pt-14 bg-gradient-to-r from-indigo-900 via-indigo-800 to-indigo-900 cursor-wait">
+			<div className=" h-12 w-12 animate-spin rounded-full border-4 border-solid border-white border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]">
+				<span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+					Loading...
+				</span>
+			</div>
+			<span className="flex justify-center w-full">
+				<span
+					ref={divMessageRef}
+					className="absolute inline-flex rounded-full font-roboto text-white text-xl text-center font-semibold uppercase
+							md:text-3xl
+					">
+					Validando usuario
+				</span>
+			</span>
+		</div>
+	);
+};
+export default Validate;
