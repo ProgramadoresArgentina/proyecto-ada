43e55879fb874264f15982f5a32e3a6c42a9a0fb
Daniela <martinezdl_@hotmail.com>
2023-07-14 20:57:45 -0300

    create events endpoint

---
+++ pages/api/events.tsx
@@ -0,0 +1,25 @@
+
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../prismaClient/db";
+
+
+export default async function events(req: NextApiRequest, res: NextApiResponse) {
+    const { method } = req;
+    switch (method) {
+        case "GET":
+            try {
+                const getEvents = await prisma.events.findMany(
+                    {
+                        where: {
+                            expiredAt: {
+                                lte: new Date() // Filtrar eventos que ocurren después de la fecha actual
+                            }
+                        }
+                    })
+                res.json({ getEvents });
+            } catch (e) {
+                res.status(200).json({ message: "ERROR_FINDING_EVENTS" });
+            }
+            break;
+    }
+}
