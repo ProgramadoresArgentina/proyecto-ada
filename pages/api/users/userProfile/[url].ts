a1757207f9c1a5b2cd4389036a0f458f7eb1ff6b
Nicolas Smael <77250452+SmaelNicolas@users.noreply.github.com>
2023-08-04 22:36:25 -0300

    PLAT-68: [url].ts - get user info to show in userProfile /pro (#71)

---
+++ pages/api/users/userProfile/[url].ts
@@ -0,0 +1,75 @@
+// /api/user/userprofile/url
+// buscar x url de userSettings
+
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../../lib/prisma";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+	const { method, body, query } = req;
+
+	switch (method) {
+		case "GET":
+			try {
+				const { url } = query;
+				const getUserID = await prisma.userSettings.findUnique({
+					where: { url: String(url) },
+					select: {
+						userId: true,
+					},
+				});
+
+				const getDataFromCv = await prisma.cv.findUnique({
+					where: { userId: getUserID.userId },
+					select: {
+						experience: true,
+						education: true,
+						certifications: true,
+					},
+				});
+
+				const getDataFromUserSettings =
+					await prisma.userSettings.findUnique({
+						where: { userId: getUserID.userId },
+						select: {
+							firstName: true,
+							lastName: true,
+							avatar: true,
+							description: true,
+						},
+					});
+
+				const getDataFromUser = await prisma.user.findUnique({
+					where: { id: getUserID.userId },
+					select: {
+						email: true,
+						createdAt: true,
+						cv: true,
+					},
+				});
+
+				const getDataFromArticles = await prisma.articles.findMany({
+					where: { userId: getUserID.userId },
+					take: 5,
+					select: {
+						title: true,
+						content: true,
+						image: true,
+						hashtags: true,
+					},
+				});
+
+				res.json({
+					getDataFromCv,
+					getDataFromUserSettings,
+					getDataFromUser,
+					getDataFromArticles,
+				});
+			} catch (error: any) {
+				res.status(200).json({
+					message: `USER_WITH_URL: ${query.url} NOT_FOUND`,
+				});
+			}
+			break;
+	}
+};
