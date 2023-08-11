3c2c28e51340ae9fc882164d9aedc19e0549b954
Smael Nicolas <nico.smael@gmail.com>
2023-08-11 08:50:23 -0300

    FIX image size, add bg color

--- components/BlogPage/Hero/ImageBg.tsx
+++ components/BlogPage/Hero/ImageBg.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 export const ImageBg: FC = () => (
 	<div
-		className="w-full h-full bg-background-blog-world bg-no-repeat bg-right-top bg-cover
+		className="w-full h-full bg-navyBlue bg-background-blog-world bg-no-repeat bg-right-top bg-cover
 					xl:w-1/2"
 	/>
 );
