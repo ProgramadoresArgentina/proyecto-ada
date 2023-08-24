54c9aa80746c690eb13543029ae1e715d91816cd
Smael Nicolas <nico.smael@gmail.com>
2023-08-24 18:25:16 -0300

    NotFound.tsx - Add option to hide register button

--- components/NotFound/NotFound.tsx
+++ components/NotFound/NotFound.tsx
@@ -1,6 +1,13 @@
 import Link from "next/link";
+import { NotFoundProps } from "../../interface/types";
 
-export const NotFound = ({ title, content, url, buttonName }) => {
+export const NotFound = ({
+	title,
+	content,
+	url,
+	buttonName,
+	showRegister = true,
+}: NotFoundProps) => {
 	return (
 		<main className="grid min-h-screen place-items-center bg-[#0D1116] px-6 py-24 sm:py-32 lg:px-8">
 			<div className="text-center flex justify-start items-center flex-col">
@@ -15,11 +22,13 @@ export const NotFound = ({ title, content, url, buttonName }) => {
 					{content}
 				</p>
 				<div className="mt-10 flex items-center justify-center gap-x-6">
-					<Link
-						href="/login"
-						className="plain-button rounded-md px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-						Registrarse
-					</Link>
+					{showRegister && (
+						<Link
+							href="/login"
+							className="plain-button rounded-md px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+							Registrarse
+						</Link>
+					)}
 					<Link
 						href={url}
 						className="text-sm font-semibold text-white">
