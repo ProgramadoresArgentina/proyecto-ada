2b29882d5ea4e6ce07e41d8966f6661f8cba009b
Nicolas Smael <77250452+SmaelNicolas@users.noreply.github.com>
2023-08-25 21:00:47 -0300

    Remove Validate page, redirects to /validate changed to / (#89)

--- pages/api/auth/[...auth0].ts
+++ pages/api/auth/[...auth0].ts
@@ -1,61 +1,66 @@
-import { handleAuth, handleLogin, handleCallback, Claims } from "@auth0/nextjs-auth0";
-import { getSession } from '@auth0/nextjs-auth0';
-import { prisma } from "../../../prismaClient/db"
+import {
+	Claims,
+	getSession,
+	handleAuth,
+	handleCallback,
+	handleLogin,
+} from "@auth0/nextjs-auth0";
+import { prisma } from "../../../prismaClient/db";
 
 const getLoginState = (req, loginOptions) => {
-    return {
-        returnTo: '/validate'
-    };
+	return {
+		returnTo: "/",
+	};
 };
 
 export default handleAuth({
-    async login(req, res) {
-        try {
-            await handleLogin(req, res, { getLoginState });
-        } catch (err) {
-            res.status(err.status ?? 500).end(err.message)
-        }
-    },
-    callback: async (req, res) => {
-      try {
-        await handleCallback(req, res, {
-          redirectUri: process.env.AUTH0_BASE_URL
-        });
-        const {user} = await getSession(req, res);
-        createUser(user);
-      } catch (error) {
-        console.error(error);
-      }
-    }
+	async login(req, res) {
+		try {
+			await handleLogin(req, res, { getLoginState });
+		} catch (err) {
+			res.status(err.status ?? 500).end(err.message);
+		}
+	},
+	callback: async (req, res) => {
+		try {
+			await handleCallback(req, res, {
+				redirectUri: process.env.AUTH0_BASE_URL,
+			});
+			const { user } = await getSession(req, res);
+			createUser(user);
+		} catch (error) {
+			console.error(error);
+		}
+	},
 });
 
-
 async function createUser(user: Claims) {
-    const userExists = await prisma.user.findUnique({ where: { email: user.email } });
-    if (!userExists) {
-        const userRow = await prisma.user.create({
-            data: {
-                email: user.email,
-                username: user.name,
-            }
-        });
-    
-        const userSettingsRow  = await prisma.userSettings.create({
-            data: {
-                avatar: user.picture || user.avatar,
-                description: 'Soy nuevo en la comunidad!',
-                firstName: '',
-                lastName: '',
-                user: {
-                    connect: {
-                        id: userRow.id
-                    }
-                }
-            }
-        }); 
-    }
-}
+	const userExists = await prisma.user.findUnique({
+		where: { email: user.email },
+	});
+	if (!userExists) {
+		const userRow = await prisma.user.create({
+			data: {
+				email: user.email,
+				username: user.name,
+			},
+		});
 
+		const userSettingsRow = await prisma.userSettings.create({
+			data: {
+				avatar: user.picture || user.avatar,
+				description: "Soy nuevo en la comunidad!",
+				firstName: "",
+				lastName: "",
+				user: {
+					connect: {
+						id: userRow.id,
+					},
+				},
+			},
+		});
+	}
+}
 
 //This creates the following routes:
 
@@ -63,4 +68,3 @@ async function createUser(user: Claims) {
 // - /api/auth/logout   -to logout
 // - /api/auth/callback  -to redirect
 // - /api/auth/me     -to check logged user
-
