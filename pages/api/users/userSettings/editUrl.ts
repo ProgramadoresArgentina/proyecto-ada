68f7a0bf2ceb5f5b62352aaa7e918372a4817475
Carolina Riddick <107819898+Carolina-Riddick@users.noreply.github.com>
2023-09-07 12:31:33 +1000

    enviar user al perfil (#98)

--- pages/api/users/userSettings/editUrl.ts
+++ pages/api/users/userSettings/editUrl.ts
@@ -10,6 +10,7 @@ export default async function editUrl(req: NextApiRequest, res: NextApiResponse)
 	const { method, body } = req;
 	const {user} = await getSession(req, res); // User is logged in 
     const userIdFromDB = await prisma.user.findUnique({ where: { email: user.email }, select: {id: true}  });
+	console.log(user);
 	if (user && method == "PUT") {
 		try {
 			let newUrl = body.data // new url
--- pages/api/users/userSettings/index.ts
+++ pages/api/users/userSettings/index.ts
@@ -1,23 +1,27 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../../../prismaClient/db";
+import { getSession } from "@auth0/nextjs-auth0";
 
-// GET api/users
-// POST api/users Create user
+// GET /api/users/userSettings
 
 export default async function userSettings(req:NextApiRequest, res:NextApiResponse) {  
-    const {method} = req;
-    switch (method) {
-        case "GET":
-            try{
-                 const getUserSettings = await prisma.userSettings.findMany(
-                {
-                     include: { user:true } 
-                })
-                res.json({ getUserSettings });
-            }catch(e)
-            {
-                res.status(200).json({ message:"ERROR_FINDING_USER_SETTINGS"});
-            }
-            break;
+    
+    const {user} = await getSession(req, res); // User is logged in 
+    const userExists = await prisma.user.findUnique({ where: { email: user.email }, select: {id: true}  });
+
+    if(userExists){
+        try{
+            const urlProfile = await prisma.userSettings.findUnique({
+                where: { userId: userExists.id},
+                select: {
+                    url : true,
+                }
+            })
+            return res.status(200).redirect(`/pro/${urlProfile}`)
+       } catch(e) {
+           res.status(500).json({ message:"ERROR_FINDING_USER_URL"});
+       }
+    } else{
+        res.status(500).json({ message:"USER_NOT_LOGGED"});
     }
 }
