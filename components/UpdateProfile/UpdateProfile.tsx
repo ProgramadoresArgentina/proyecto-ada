9e417fa2e2d3387e21bc4bd443226476dc6a481b
Nicolas Smael <77250452+SmaelNicolas@users.noreply.github.com>
2023-09-15 20:44:07 -0300

    PLAT-92 : Update profile modal (#100)

---
+++ components/UpdateProfile/UpdateProfile.tsx
@@ -0,0 +1,311 @@
+import { useEffect, useRef, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import {
+	linkSocials,
+	listPositions,
+	senioritys,
+	status,
+} from "../../constants/constants";
+import { launchToast } from "../../helpers/launchToast";
+import { useFetch } from "../../hooks/useFetch";
+import {
+	ErrorMsgState,
+	UpdateProfileProps,
+	UserProfileProps,
+} from "../../interface/types";
+import { AccordionRadioButton } from "./AccordionRadioButton";
+import { HeaderDropdown } from "./HeaderDropdown";
+
+const regex = /^[a-zA-Z0-9]*$/;
+
+export const UpdateProfile = ({
+	openDialog,
+	handleDialog,
+}: UpdateProfileProps) => {
+	const { data, error, fetchData } = useFetch();
+	const dialogRef = useRef(null);
+	const [errorMsg, setErrorMsg] = useState<ErrorMsgState>({
+		url: "",
+		description: "",
+		position: "",
+	});
+	const [positions, setPositions] = useState<string[]>([]);
+
+	const [userProfile, setUserProfile] = useState<UserProfileProps>({
+		linkedin: "",
+		github: "",
+		behance: "",
+		portfolio: "",
+		seniority: "junior",
+		status: "miembro",
+		position: "",
+		url: "",
+		description: "Miembro de Programadores Argentina",
+	});
+
+	const handleOnChange = (event: any) => {
+		const inputValue = event.target.value;
+		const inputName = event.target.name;
+		if (!inputName) return;
+		if (inputName === "position") {
+			if (positions.includes(inputValue)) {
+				setPositions(positions.filter((pos) => pos !== inputValue));
+			} else {
+				if (positions.length < 4) {
+					setPositions([...positions, inputValue]);
+				}
+			}
+		} else {
+			setUserProfile({
+				...userProfile,
+				[inputName]: inputValue,
+			});
+		}
+	};
+
+	useEffect(() => {
+		setErrorMsg({
+			...errorMsg,
+			position: `${positions.length}/4`,
+		});
+	}, [positions]);
+
+	const handleBlur = (event) => {
+		const inputValue = event.target.value;
+		const inputName = event.target.name;
+
+		if (inputName === "url") {
+			!regex.test(inputValue) ||
+			inputValue.length > 25 ||
+			inputValue.length < 3
+				? setErrorMsg({
+						...errorMsg,
+						[inputName]:
+							"* Entre 3 y 25 caracteres que contengan solo letras y números *",
+				  })
+				: setErrorMsg({
+						...errorMsg,
+						[inputName]: "",
+				  });
+			setUserProfile({ ...userProfile, [inputName]: inputValue });
+		}
+		if (inputName === "description") {
+			inputValue.length > 250 || inputValue.length < 3
+				? setErrorMsg({
+						...errorMsg,
+						[inputName]:
+							"* Entre 3 y 250 caracteres que contengan solo letras y números *",
+				  })
+				: setErrorMsg({
+						...errorMsg,
+						[inputName]: "",
+				  });
+			setUserProfile({ ...userProfile, [inputName]: inputValue });
+		}
+	};
+
+	const handleSubmit = (e: any) => {
+		e.preventDefault();
+		let body = {
+			...userProfile,
+			position: positions.join(" - "),
+		};
+		console.table(body);
+		console.log(body.position);
+
+		// fetchData("/api/users/userSettings/editUrl", "PUT", body);
+	};
+
+	// USEEFFECT PARA  CERRAR EL MODAL Y LANZAR UN TOAST
+	useEffect(() => {
+		if (data) {
+			launchToast("success", "URL actualizada");
+			setTimeout(() => {
+				dialogRef.current.close();
+			}, 1000);
+		}
+		if (error) {
+			launchToast("error", "Error al actualizar la URL");
+		}
+
+		return () => {
+			toast.dismiss();
+		};
+	}, [data, error]);
+
+	// USEEFFECT PARA  ABRIR EL MODAL Y CERRAR EL MODAL
+	useEffect(() => {
+		openDialog ? dialogRef.current.showModal() : dialogRef.current.close();
+	}, [openDialog]);
+
+	// !TODO - USEEFFECT QUE TRAERIA LOS DATOS DEL USUARIO LOGEADO
+	// PARA ACTUALIZAR LOS CAMPOS
+	useEffect(() => {
+		// console.log("ABRE MODAL");
+		// console.log("TRAE LOS DATOS");
+		// console.log("RELLENA LOS CAMPOS");
+	}, []);
+
+	return (
+		<dialog
+			ref={dialogRef}
+			onClose={handleDialog}
+			className="w-screen h-screen shadow-2xl backdrop-blur-md bg-white/20 max-w-full max-h-full no-scrollbar">
+			<div
+				onClick={handleDialog}
+				className="m-auto w-full h-full flex flex-col justify-start items-center pt-[150px]">
+				<form
+					onClick={(e) => e.stopPropagation()}
+					onSubmit={handleSubmit}
+					className="bg-[#0D1116] w-full max-w-[600px] px-8 pt-6 pb-8 mb-4 rounded-md xl:px-4 xl:pt-3 xl:pb-4 xl:mb-2 ">
+					{/* EXPERIENCIA */}
+					<AccordionRadioButton
+						title="Experiencia"
+						options={senioritys}
+						radioGroup="seniority"
+						userProp={userProfile.seniority}
+						handleChange={handleOnChange}
+					/>
+					<div className="h-[1px] w-full bg-slate-500 my-2" />
+					{/* FIN EXPERIENCIA */}
+					{/* ------------------------------ */}
+					{/* STATUS */}
+					<AccordionRadioButton
+						title="Status"
+						options={status}
+						radioGroup="status"
+						userProp={userProfile.status}
+						handleChange={handleOnChange}
+					/>
+					<div className="h-[1px] w-full bg-slate-500 my-2" />
+					{/* FIN STATUS */}
+					{/* ------------------------------ */}
+					{/* POSITION */}
+					<details className="group flex flex-col justify-start items-center transition ease-in-out delay-150 py-2">
+						<HeaderDropdown
+							title={`Posición - ${errorMsg.position}`}>
+							{positions.length >= 4 && (
+								<button
+									onClick={() => setPositions([])}
+									className="text-red-600 pl-4">
+									❌ Reset
+								</button>
+							)}
+						</HeaderDropdown>
+						<div className="text-neutral-600 mt-3 group-open:animate-fadeIn h-[200px] overflow-y-scroll no-scrollbar">
+							{listPositions.map(({ id, name }) => (
+								<div
+									key={id}
+									className="flex items-center w-full pl-4 my-2 gap-2 bg-gray-100 rounded-md hover:bg-orangePA cursor-pointer">
+									<input
+										id={id.toString()}
+										type="checkbox"
+										name="position"
+										value={name}
+										className="w-4 h-4 focus:accent-indigo-800 accent-indigo-800 cursor-pointer"
+										onChange={handleOnChange}
+										disabled={positions.length >= 4}
+										checked={positions.includes(name)}
+									/>
+									<label
+										htmlFor={id.toString()}
+										className="ml-2 py-2 w-full h-full text-base font-medium text-gray-900 cursor-pointer">
+										{name}
+									</label>
+								</div>
+							))}
+						</div>
+					</details>
+					<div className="h-[1px] w-full bg-slate-500 my-2" />
+					{/* FIN POSITION */}
+					{/* ------------------------------ */}
+					{/* URL */}
+					<div className="mb-2 flex flex-col justify-start items-start gap-1 pb-2">
+						<label
+							className="leading-8 text-gray-100 flex flex-col w-full justify-start items-start min-h-[50px]
+							sm:flex-row sm:justify-start sm:items-center sm:gap-3 sm:min-h-0">
+							<div className="w-max">URL Profile</div>
+							<p className="text-red-500 text-xs italic  w-max">
+								{errorMsg.url}
+							</p>
+						</label>
+						<input
+							className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:shadow-outline bg-gray-100 text-sm"
+							name="url"
+							type="text"
+							placeholder="programadoresargentina.com/pro/...."
+							onChange={handleOnChange}
+							value={userProfile.url}
+							onBlur={handleBlur}
+						/>
+					</div>
+					<div className="h-[1px] w-full bg-slate-500 my-2" />
+					{/* FIN URL */}
+					{/* ------------------------------ */}
+					{/* DESCRIPCION */}
+					<div className="mb-2 flex flex-col justify-start items-start gap-1 pb-2">
+						<label
+							className="leading-8 text-gray-100 flex flex-col w-full justify-start items-start min-h-[50px]
+							sm:flex-row sm:justify-start sm:items-center sm:gap-3 sm:min-h-0">
+							<div className="w-max">Descripción</div>
+							<p className="text-red-500 text-xs italic  w-max">
+								{errorMsg.description}
+							</p>
+						</label>
+						<textarea
+							className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:shadow-outline bg-gray-100 text-sm resize-none"
+							name="description"
+							onChange={handleOnChange}
+							value={userProfile.description}
+							onBlur={handleBlur}
+						/>
+					</div>
+					<div className="h-[1px] w-full bg-slate-500 my-2" />
+					{/* FIN DESCRIPCION */}
+					{/* ------------------------------ */}
+					{/* REDES */}
+					<details className="group flex flex-col justify-start items-center transition ease-in-out delay-150 py-2">
+						<HeaderDropdown title="Redes" />
+						<div className="text-neutral-600 mt-3 group-open:animate-fadeIn">
+							{linkSocials.map(({ id, value, link }) => (
+								<div
+									key={id}
+									className="relative mb-4 flex flex-wrap items-stretch rounded-md border">
+									<label
+										htmlFor={value}
+										className="w-[130px] text-gray-100 py-2 px-2 text-right cursor-pointer">
+										{link}
+									</label>
+									<input
+										id={value}
+										type="text"
+										name={value}
+										className="relative m-0 block w-[1px] min-w-0 flex-auto shadow appearance-none border rounded-r-sm py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:shadow-outline bg-gray-100 text-sm"
+										onChange={handleOnChange}
+									/>
+								</div>
+							))}
+						</div>
+					</details>
+					<div className="h-[1px] w-full bg-slate-500 my-2" />
+					{/* FIN REDES */}
+					{/* ------------------------------ */}
+					<div className="flex items-center justify-between gap-1 mt-3">
+						<button
+							onClick={() => dialogRef.current.close()}
+							className="bg-[#F78001] hover:bg-red-400 text-gray-800 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+							type="button">
+							Cerrar
+						</button>
+						<button
+							className="text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline plain-button"
+							type="submit">
+							Actualizar
+						</button>
+					</div>
+				</form>
+			</div>
+			<ToastContainer />
+		</dialog>
+	);
+};
--- constants/constants.tsx
+++ constants/constants.tsx
@@ -8,3 +8,291 @@ export const LINKS_URL_SOCIALS = {
 	youtube: "",
 	twitter: "",
 };
+
+export const linkSocials = [
+	{ id: 1, value: "linkedin", link: "linkedin.com/in/" },
+	{ id: 2, value: "github", link: "github.com/" },
+	{ id: 3, value: "behance", link: "behance.net/" },
+	{ id: 4, value: "portfolio", link: "Porfolio" },
+];
+
+export const senioritys = [
+	{ id: 1, name: "Trainee", value: "trainee" },
+	{ id: 2, name: "Junior", value: "junior" },
+	{ id: 3, name: "Semi Senior", value: "semisenior" },
+	{ id: 4, name: "Senior", value: "senior" },
+];
+
+export const status = [
+	{ id: 1, name: "Miembro", value: "miembro" },
+	{ id: 2, name: "Buscando", value: "buscando" },
+	{ id: 3, name: "Contratando", value: "contratando" },
+];
+
+export const listPositions = [
+	{
+		id: 42,
+		name: "AI Engineer",
+		value: "ai engineer",
+	},
+	{
+		id: 53,
+		name: "AI Researcher",
+		value: "ai researcher",
+	},
+	{
+		id: 22,
+		name: "AI/Machine Learning Engineer",
+		value: "ai/machine learning engineer",
+	},
+	{
+		id: 2,
+		name: "Back-End",
+		value: "back-end",
+	},
+	{
+		id: 24,
+		name: "Back-End Developer",
+		value: "back-end developer",
+	},
+	{
+		id: 52,
+		name: "Blockchain Developer",
+		value: "blockchain developer",
+	},
+	{
+		id: 15,
+		name: "Business Analyst",
+		value: "business analyst",
+	},
+	{
+		id: 9,
+		name: "Cloud Architect",
+		value: "cloud architect",
+	},
+	{
+		id: 21,
+		name: "Cloud Engineer",
+		value: "cloud engineer",
+	},
+	{
+		id: 27,
+		name: "Cloud Solutions Architect",
+		value: "cloud solutions architect",
+	},
+	{
+		id: 59,
+		name: "Cloud Solutions Engineer",
+		value: "cloud solutions engineer",
+	},
+	{
+		id: 39,
+		name: "Cloud Specialist",
+		value: "cloud specialist",
+	},
+	{
+		id: 56,
+		name: "Cybersecurity Analyst",
+		value: "cybersecurity analyst",
+	},
+	{
+		id: 20,
+		name: "Cybersecurity Specialist",
+		value: "cybersecurity specialist",
+	},
+	{
+		id: 12,
+		name: "Data Analyst",
+		value: "data analyst",
+	},
+	{
+		id: 38,
+		name: "Data Engineer",
+		value: "data engineer",
+	},
+	{
+		id: 26,
+		name: "Data Scientist",
+		value: "data scientist",
+	},
+	{
+		id: 3,
+		name: "Database Administrator",
+		value: "database administrator",
+	},
+	{
+		id: 19,
+		name: "Database Developer",
+		value: "database developer",
+	},
+	{
+		id: 4,
+		name: "DevOps Engineer",
+		value: "devops engineer",
+	},
+	{
+		id: 31,
+		name: "DevOps Specialist",
+		value: "devops specialist",
+	},
+	{
+		id: 1,
+		name: "Front-End",
+		value: "front-end",
+	},
+	{
+		id: 23,
+		name: "Front-End Developer",
+		value: "front-end developer",
+	},
+	{
+		id: 5,
+		name: "Full-Stack Developer",
+		value: "full-stack developer",
+	},
+	{
+		id: 51,
+		name: "IT Administrator",
+		value: "it administrator",
+	},
+	{
+		id: 99,
+		name: "IT Analyst",
+		value: "it analyst",
+	},
+	{
+		id: 35,
+		name: "IT Consultant",
+		value: "it consultant",
+	},
+	{
+		id: 28,
+		name: "IT Manager",
+		value: "it manager",
+	},
+	{
+		id: 58,
+		name: "IT Project Manager",
+		value: "it project manager",
+	},
+	{
+		id: 16,
+		name: "IT Support Specialist",
+		value: "it support specialist",
+	},
+	{
+		id: 61,
+		name: "Machine Learning Engineer",
+		value: "machine learning engineer",
+	},
+	{
+		id: 55,
+		name: "Machine Learning Specialist",
+		value: "machine learning specialist",
+	},
+	{
+		id: 25,
+		name: "Mobile App Developer",
+		value: "mobile app developer",
+	},
+	{
+		id: 94,
+		name: "Mobile Developer",
+		value: "mobile developer",
+	},
+	{
+		id: 14,
+		name: "Network Administrator",
+		value: "network administrator",
+	},
+	{
+		id: 97,
+		name: "Network Architect",
+		value: "network architect",
+	},
+	{
+		id: 7,
+		name: "Network Engineer",
+		value: "network engineer",
+	},
+	{
+		id: 30,
+		name: "Network Security Engineer",
+		value: "network security engineer",
+	},
+	{
+		id: 13,
+		name: "Project Manager",
+		value: "project manager",
+	},
+	{
+		id: 32,
+		name: "QA Engineer",
+		value: "qa engineer",
+	},
+	{
+		id: 8,
+		name: "Security Analyst",
+		value: "security analyst",
+	},
+	{
+		id: 41,
+		name: "Security Engineer",
+		value: "security engineer",
+	},
+	{
+		id: 54,
+		name: "Software Architect",
+		value: "software architect",
+	},
+	{
+		id: 36,
+		name: "Software Developer",
+		value: "software developer",
+	},
+	{
+		id: 100,
+		name: "Software Development Manager",
+		value: "software development manager",
+	},
+	{
+		id: 18,
+		name: "Software Engineer",
+		value: "software engineer",
+	},
+	{
+		id: 11,
+		name: "Software Tester",
+		value: "software tester",
+	},
+	{
+		id: 6,
+		name: "System Administrator",
+		value: "system administrator",
+	},
+	{
+		id: 34,
+		name: "Systems Analyst",
+		value: "systems analyst",
+	},
+	{
+		id: 29,
+		name: "Technical Writer",
+		value: "technical writer",
+	},
+	{
+		id: 10,
+		name: "UI/UX Designer",
+		value: "ui/ux designer",
+	},
+	{
+		id: 37,
+		name: "Web Designer",
+		value: "web designer",
+	},
+	{
+		id: 17,
+		name: "Web Developer",
+		value: "web developer",
+	},
+];
