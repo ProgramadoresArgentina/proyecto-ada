4af48dc94ff0274c96f88298f15d5587f53e1bcf
Daniela <martinezdl_@hotmail.com>
2023-11-17 19:34:21 -0300

    add new styles to talents marquee

--- components/TalentsSection/TalentsMarquee.tsx
+++ components/TalentsSection/TalentsMarquee.tsx
@@ -2,13 +2,17 @@ import React from 'react'
 import TalentsCard from './TalentCards'
 import Marquee from 'react-fast-marquee'
 import { FaMedal } from 'react-icons/fa';
-
+ {/* cards de talentos destacados */}
 const TalentsMarquee = () => {
   return (
     <div className="bg-gradient-to-r from-zinc-900 to-slate-900 w-full h-auto justify-center items-center">
      <div>
-      <h1 className="flex justify-center  text-xl font-bold mt-10 text-golden">Destacados del mes</h1>
-      {/* cards de destacados */}
+     <div className="flex items-center justify-center mt-10">
+  <hr className="border-t-2 border-golden flex-grow mx-4" />
+  <h1 className="text-2xl font-semibold text-golden">Destacados del mes</h1>
+  <hr className="border-t-2 border-golden flex-grow mx-4" />
+</div>
+
             <Marquee speed={30}  pauseOnHover={true} autoFill={true}>
             <TalentsCard
             Medal={<FaMedal color="#ffb703"/>}
@@ -22,23 +26,7 @@ const TalentsMarquee = () => {
             />
         </Marquee>
       </div>
-   
-      <h1 className='text-2xl font-bold text-center text-white'>
-      Hace realidad tu proximo  <span className='degrade-text'> proyecto</span>
-        </h1>
-        {/* cards standard */}
-        <Marquee speed={10}  pauseOnHover={true} autoFill={true}>
-          <TalentsCard
-          MainCardsStyle={"max-w-sm rounded overflow-hidden shadow-lg bg-indigo-800 transition-transform transform hover:-translate-y-2 hover:scale-105 mx-6 relative w-200 h-300"}
-          CardColor={"bg-white  w-full mt-10 px-5 py-3 flex flex-col justify-between text-black"}
-          ImageColors={"w-20 h-20 rounded-full border-4 border-white absolute top-20 left-1/2 transform -translate-x-1/2      -translate-y-1/2"}
-          name={"Daniela Martinez"}
-          years={"2 yrs"}
-          seniority={"Junior"}
-          position={"Front-end"}
-          />
-        </Marquee>
-    </div>
+ </div>
   )
 }
 export default TalentsMarquee
