c02da66e527e9ad21e434c294862360fac205820
Carolina Riddick <107819898+Carolina-Riddick@users.noreply.github.com>
2023-11-25 10:09:23 +1100

    updated API created_at and expired_at (#116)

--- pages/api/events.tsx
+++ pages/api/events.tsx
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../prismaClient/db";
 
 
-
+// NO devuelve eventos expirados
 export default async function events(req: NextApiRequest, res: NextApiResponse) {
     const { method } = req;
     switch (method) {
@@ -11,8 +11,11 @@ export default async function events(req: NextApiRequest, res: NextApiResponse) 
                 const getEvents = await prisma.events.findMany(
                     {
                         where: {
+                            createdAt : {
+                                lt: new Date(), // Evento que tengan como fecha de creacion ANTES de la fecha actual
+                            },
                             expiredAt: {
-                                gte: new Date() // Filtrar eventos que ocurren después de la fecha actual
+                                gte: new Date() // Eventos que expiren DESPUES o IGUAL de la fecha actual
                             }
                         }
                     })
